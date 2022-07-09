5d78f81d54001dfa1066a105876ba56e8dc1523d
AA Student <appacademy@Mini-23.local>
2022-07-08 17:04:54 -0700

    windowed-phase4-done

--- windowed_max_range.rb
+++ windowed_max_range.rb
@@ -26,7 +26,7 @@ end
 # p windowed_max_range([1, 0, 2, 5, 4, 8], 4) == 6 # 2, 5, 4, 8
 # p windowed_max_range([1, 3, 2, 5, 4, 8], 5) == 6 # 3, 2, 5, 4, 8
 
-class myQueue
+class MyQueue
     # attr_reader :store
     def initialize
         @store = []
@@ -47,20 +47,60 @@ class myQueue
         @store.unshift
     end
 end
+#enqueue is O(n)
+#dequeue is O(1)
+#all other methods are O(1)
 
-def windowed_max_range(arr, w)
-    queue = myQueue.new
-    i = 0
-    while i < w
-        enqueue(arr[i])
-        i += 1
+class MyStack 
+    def initialize
+        @store = []
     end
-    while i < arr.length - w + 1
-
+    def peek
+        @store[-1]
+    end
+    def size
+        @store.length
+    end
+    def empty
+        @store.empty?
+    end
+    def push(ele)
+        @store.push(ele)
+    end
+    def pop
+        @store.pop
+    end
 end
-# p windowed_max_range([1, 0, 2, 5, 4, 8], 2) == 4 # 4, 8
-# p windowed_max_range([1, 0, 2, 5, 4, 8], 3) == 5 # 0, 2, 5
-# p windowed_max_range([1, 0, 2, 5, 4, 8], 4) == 6 # 2, 5, 4, 8
-# p windowed_max_range([1, 3, 2, 5, 4, 8], 5) == 6 # 3, 2, 5, 4, 8
-#O(n^2) in time 
-#O(n) in space
+
+class StackQueue #[1,2,3,4,5] [1,2,3,4,5]
+    #[]
+    def initialize
+        queue_stack = MyStack.new
+        temp_stack = MyStack.new
+    end
+
+    def size(stack)
+        stack.length
+    end
+    def empty?(stack)
+        stack.empty?
+    end
+    def enqueue(big_ele)
+        while !queue_stack.empty?
+            ele = queue_stack.pop
+            temp_stack.push(ele)
+        end
+        queue_stack.push(big_ele)
+        while !temp_stack.empty?
+            ele2 = temp_stack.pop
+            queue_stack.push(ele2)
+        end
+    end
+    def dequeue
+        if queue_stack.empty?
+            print "Queue is empty"
+            return
+        end
+        queue_stack.pop
+    end
+end
